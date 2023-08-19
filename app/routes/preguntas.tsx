64729e4640342bf5475ffe6d39ed7a89bb21386c
Diego Balado <jdiegomdq@gmail.com>
2023-08-19 13:13:34 -0300

    moves data loading back to server

--- app/routes/preguntas.tsx
+++ app/routes/preguntas.tsx
@@ -1,26 +1,33 @@
 import { useState } from "react";
 import MultipleChoice from "~/components/MultipleChoice";
 import preguntas from "../../data/preguntas.json";
+import { useLoaderData } from "@remix-run/react";
+
+const MAX_OPTIONS = 10;
+
+export function loader() {
+  const data = Array.from({ length: MAX_OPTIONS }, (_, index) => {
+    const rndInt = Math.floor(Math.random() * preguntas?.length);
+    return preguntas.splice(rndInt, 1)[0];
+  });
+
+  return { data };
+}
 
 export default function Preguntas() {
-  const MAX_OPTIONS = 10;
   const [currentItem, setCurrentItem] = useState(0);
   const [success, setSuccess] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
   const [showResult, setShowResult] = useState(false);
   const [score, setScore] = useState(0);
-  const pregs = [...preguntas];
-  const data = Array.from({ length: MAX_OPTIONS }, (_, index) => {
-    const rndInt = Math.floor(Math.random() * (pregs?.length - 1)) + 1;
-    const newOption = pregs?.splice(rndInt, 1)[0];
-    return newOption;
-  });
-  const item = data?.[0];
-  const message = success ? "Buenaaa" : "Dale, mamert@, andá a estudiar";
+  const { data } = useLoaderData();
+  const item = data?.[currentItem];
 
+  const message = success ? "Buenaaa" : "Dale, mamert@, andá a estudiar";
+  console.log({ data });
   return (
     <div className="h-screen bg-gradient-to-r from-slate-900 to-slate-700 wrapper">
-      <div className="box">
+      <div className="box -z-10">
         <div></div>
         <div></div>
         <div></div>
@@ -33,7 +40,7 @@ export default function Preguntas() {
         <div></div>
       </div>
       {showAlert && (
-        <div className="fixed flex items-center justify-center bg-slate-800 bg-opacity-60 w-full h-full z-20">
+        <div className="fixed flex items-center justify-center bg-slate-800 bg-opacity-60 w-full h-full">
           <div className="alert w-80 shadow-lg flex flex-col content-between top-1/4">
             <div>
               <h3 className="font-bold">{message}</h3>
@@ -62,21 +69,21 @@ export default function Preguntas() {
         `}
         </div>
       ) : (
-        <div className="flex flex-col justify-start items-center h-screen">
+        <div className="flex flex-col justify-start items-center h-screen relative">
           <h1 className="text-center pt-0 text-2xl mt-6 text-slate-50 uppercase font-black">
             Bienvenid@ a TotoJuegos!!
           </h1>
           <div className="bg-gray-100 border-2 border-gray-300 mt-6 p-4 rounded-xl w-auto text-center">
-            {data[currentItem]?.pregunta}
+            {item?.pregunta}
           </div>
           <MultipleChoice
             options={[
-              data[currentItem]?.opcion_1,
-              data[currentItem]?.opcion_2,
-              data[currentItem]?.opcion_3,
-              data[currentItem]?.opcion_4,
+              item?.opcion_1,
+              item?.opcion_2,
+              item?.opcion_3,
+              item?.opcion_4,
             ]}
-            answer={data[currentItem]?.respuesta}
+            answer={item?.respuesta}
             onResponse={(response) => {
               setSuccess(response);
               if (response) setScore(score + 1);
